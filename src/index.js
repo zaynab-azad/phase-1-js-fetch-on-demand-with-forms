13ec79ec8413e251ecc713851b3eee199dcb6442
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-24 12:15:23 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        if (data.title) {
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        } else {
+          title.innerText = "Movie not found";
+        }
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
